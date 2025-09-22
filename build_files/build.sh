4ef21a54be2668cc14dbe4494dcd26b0d5490496
Martin Kleinberger <martin.kleinberger@gmail.com>
2025-09-22 22:32:09 +0100

    new build file

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,52 +1,29 @@
 #!/bin/bash
 
+# This script is designed to be idempotent and can be run safely multiple times.
 set -ouex pipefail
 
-### Install packages
+## -- SYSTEM CONFIGURATION -- ##
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
+# Enable podman socket for running rootless containers
 systemctl enable podman.socket
 
+## -- REPOSITORY SETUP -- ##
 
-### my changes
-
-# Enable Google Chrome repo
+# Enable Google Chrome repository
 sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/google-chrome.repo
 
+# Enable negativo17 multimedia repository and set its priority
+sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+echo 'priority=90' | tee -a /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
-#todo:
-# - move to base image
-#Podman Desktop (io.podman_desktop.PodmanDesktop)
-#Cockpit Client (org.cockpit_project.CockpitClient)
-#GNOME Builder (org.gnome.Builder)
-#Arduino IDE (cc.arduino.IDE2)
-#Sysd Manager (io.github.plrigaux.sysd-manager)
-#DevToolbox (me.iepure.devtoolbox)
-# - maybe move to base image
-#Geany (org.geany.Geany)
-#DBeaver (io.dbeaver.DBeaverCommunity)
-#GitKraken (com.axosoft.GitKraken)
-#LACT (io.github.ilya_zlobintsev.LACT)
-#Solaar (io.github.pwr_solaar.solaar)
+# Set priority for the built-in RPM Fusion repositories
+sed -i -e '$apriority=99' /etc/yum.repos.d/rpmfusion-*.repo
 
-# Packages
+## -- PACKAGE INSTALLATION -- ##
 
+# Define lists of packages to install
+# This makes it easy to manage and see what's being added.
 base_packages=(
   "google-chrome-beta"
 )
@@ -55,11 +32,22 @@ utility_packages=(
   "scrcpy"
 )
 
-packages=(
+# Combine all package lists into one
+packages_to_install=(
   ${base_packages[@]}
   ${utility_packages[@]}
 )
 
-# install rpms
-dnf5 install -y ${packages[@]}
+# Install all defined packages from the enabled repositories
+dnf5 install -y ${packages_to_install[@]}
 
+## -- TODO for your base image -- ##
+# The following are your notes on packages to consider moving into your
+# declarative build file for a truly custom base image.
+
+# High-priority candidates:
+# Podman Desktop, Cockpit Client, GNOME Builder, Arduino IDE,
+# Sysd Manager, DevToolbox
+
+# Lower-priority candidates (depends on workflow):
+# Geany, DBeaver, GitKraken, LACT, Solaar
